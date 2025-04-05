5b313d458e83d4f5ba2da3f46dffa36bbf00cdd3
Sharankumar K <168968512+Sharan-Kumar-k@users.noreply.github.com>
2025-04-05 20:39:53 +0530

    Create greatestOfTwo.java

---
+++ number-programs/greatestOfTwo.java
@@ -0,0 +1,11 @@
+public class greatestOfTwo {
+    public static void main(String[] args) {
+        int a = 25, b = 30;
+        if (a > b)
+            System.out.println(a + " is greater");
+        else if (b > a)
+            System.out.println(b + " is greater");
+        else
+            System.out.println("Both are equal");
+    }
+}
