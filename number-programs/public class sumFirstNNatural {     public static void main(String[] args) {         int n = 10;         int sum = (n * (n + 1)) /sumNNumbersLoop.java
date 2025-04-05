f7ee0691fce70b7f4cc38971f8c2e54eeaa6c917
Sharankumar K <168968512+Sharan-Kumar-k@users.noreply.github.com>
2025-04-05 20:35:11 +0530

    Delete number-programs/public class sumFirstNNatural {     public static void main(String[] args) {         int n = 10;         int sum = (n * (n + 1)) /sumNNumbersLoop.java

--- number-programs/public class sumFirstNNatural {     public static void main(String[] args) {         int n = 10;         int sum = (n * (n + 1)) /sumNNumbersLoop.java
+++
@@ -1,7 +0,0 @@
-public class sumFirstNNatural {
-    public static void main(String[] args) {
-        int n = 10;
-        int sum = (n * (n + 1)) / 2;
-        System.out.println("Sum = " + sum);
-    }
-}
