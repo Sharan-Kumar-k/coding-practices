53e20770343277ffb7136085acb7b6b6ecf2e90a
Sharankumar K <168968512+Sharan-Kumar-k@users.noreply.github.com>
2025-04-05 20:14:44 +0530

    Create PositiveOrNegative.java

---
+++ number-programs/PositiveOrNegative.java
@@ -0,0 +1,12 @@
+// Check if number is positive, negative, or zero
+public class positiveOrNegative {
+    public static void main(String[] args) {
+        int num = -5;
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
