dedf2fb793a6d8dae1216f21cf7597e12f04f34f
Sharankumar K <168968512+Sharan-Kumar-k@users.noreply.github.com>
2025-04-05 20:27:27 +0530

    Update evenOrOdd.java

--- number-programs/number-programs/evenOrOdd.java
+++ number-programs/number-programs/evenOrOdd.java
@@ -1,9 +1,1 @@
-public class evenOrOdd {
-    public static void main(String[] args) {
-        int num = 7;
-        if (num % 2 == 0)
-            System.out.println(num + " is Even");
-        else
-            System.out.println(num + " is Odd");
-    }
-}
+
